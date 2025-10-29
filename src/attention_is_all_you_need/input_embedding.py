bba297e7224778a5c6e3dd4a536eee911e9451b1
YeonguChoe <yeongu.choe@icloud.com>
2025-10-29 15:27:19 -0400

    Update input_embedding.py

--- src/attention_is_all_you_need/input_embedding.py
+++ src/attention_is_all_you_need/input_embedding.py
@@ -9,3 +9,10 @@ def tokenize(s:str):
     tokenizer.train(files=["wiki.train.raw"], trainer=trainer)
     tokenizer.save("vocab.json")
     return tokenizer.encode(s).ids
+
+def input_embedding(sequence: torch.Tensor)->torch.Tensor:
+  vocabulary_size=30000
+  d_model = 512
+  embedding = nn.Embedding(num_embeddings=vocabulary_size, embedding_dim=d_model)
+  embedded_sequence = embedding(sequence)
+  return embedded_sequence
