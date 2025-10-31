8407fdc9eeee1a49618baa9061161e5510565db9
YeonguChoe <yeongu.choe@icloud.com>
2025-10-31 11:42:54 -0400

    positional encoding

--- src/attention_is_all_you_need/positional_encoding.py
+++ src/attention_is_all_you_need/positional_encoding.py
@@ -1,4 +1,16 @@
-# Positional Encoding: Attach position information in a sentence.
+import torch
+import math
 
-def positional_encoding():
-    pass
+def positional_encoding(batch: torch.Tensor) -> torch.Tensor:
+    result = []
+    d_model = 512
+    for position in range(batch.size(1)):
+        PE = []
+        for i in range(d_model):
+            if i % 2 == 0:
+                PE.append(math.sin(position / 10000 ** (2 * i / d_model)))
+            else:
+                PE.append(math.cos(position / 10000 ** (2 * i / d_model)))
+        result.append(torch.tensor(PE))
+    
+    return torch.stack(result, dim=0).unsqueeze(0)
